d9a649d6ddbe4f449fb9e97df7c1a43b34a2b87f
akchjnj <akechijunji@gmail.com>
2025-10-07 22:56:13 +0900

    ルーティングとテンプレート機能を追加

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Book Finder is running!"
+    return render_template("index.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
