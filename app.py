97ab2ceacc2183d3b6a071bf4004f1da59f1a85b
akchjnj <akechijunji@gmail.com>
2025-10-09 00:14:15 +0900

    フォーム入力と結果表示機能を追加

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html", title="Home")
+
+@app.route("/search",methods=["GET","POST"])
+def search():
+    if request.method == 'POST':
+        book_title = request.form.get('book_title')
+        return render_template("result.html", title="Result", book_title=book_title)
+    return render_template("search.html", title="Search")
 
 @app.route("/about")
 def about():
