d01f467f128f3a135320ad1f07140bf1b35e1450
akchjnj <akechijunji@gmail.com>
2025-10-11 16:55:38 +0900

    複数検索結果の表示／ISBN検索を追加

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import requests
 import os
 from dotenv import load_dotenv
-from urllib3 import response
 
 # 環境変数読み込み
 load_dotenv()
@@ -18,37 +17,62 @@ def home():
 @app.route("/search",methods=["GET","POST"])
 def search():
     if request.method == 'POST':
-        book_title = request.form.get('book_title')
+        search_query = request.form.get('book_title')
+        search_type = request.form.get('search_type','title')
+
+        if search_query == 'isbn':
+            q = f'isbn:{search_query}'
+        else:
+            q = search_query
 
         # Books API リクエスト
         params = {
-            'q' : book_title,
+            'q' : q,
             'key' : GOOGLE_BOOK_API_KEY,
-            'maxResults' : 1
+            'maxResults' : 10
         }
         try:
             response = requests.get(GOOGLE_BOOK_API_URL,params=params)
             response.raise_for_status()
             data = response.json()
+            print(f"Response data: {data}")  # デバッグ用
             
+            books = []
+
             # データがあるか確認
             if data.get('totalItems',0) > 0:
-                book = data['items'][0]['volumeInfo']
-                book_data = {
-                    'title': book.get('title','不明'),
-                    'authors': book.get('authors','不明'),
-                    'publisher': book.get('publisher','不明'),
-                    'thumbnail': book.get('imageLinks',{}).get('thumbnail','')
-                }
+                for item in data.get('items',[]):
+                    volume_info = item.get('volumeInfo',{})
+                    isbn_13 = None
+                    isbn_10 = None
+                    for identifiers in volume_info.get('industryIdentifiers',{}):
+                        if identifiers['type'] == 'ISBN_13':
+                            isbn_13 = identifiers['identifier']
+                        elif identifiers['type'] == 'ISBN_10':
+                            isbn_10 = identifiers['identifier']
+                    
+
+                    book_data = {
+                        'title': volume_info.get('title','不明'),
+                        'authors': ', '.join(volume_info.get('authors','不明')),
+                        'publisher': volume_info.get('publisher','不明'),
+                        'published_date': volume_info.get('publishedDate','不明'),
+                        'thumbnail': volume_info.get('imageLinks',{}).get('thumbnail',''),
+                        'isbn_13': isbn_13,
+                        'isbn_10': isbn_10,
+                        'description': volume_info.get('description','')[:200] + '...' if volume_info.get('description') else '説明なし'
+                    }
+                    books.append(book_data)
                 return render_template("result.html", 
-                                    title="Result",
-                                    book = book_data, 
-                                    search_query=book_title)
+                                        title="Result",
+                                        books = books,
+                                        total_items = data.get('totalItems',0),
+                                        search_query=search_query)
         except requests.exceptions.RequestException as e:
             return render_template('result.html',
                                     title = "Error",
                                     error = f'APIエラー：{str(e)}',
-                                    search_query = book_title)
+                                    search_query = search_query)
     return render_template("search.html", title="Search")
 
 @app.route("/about")
---
+++ templates/gb_res_sample.py
@@ -0,0 +1,83 @@
+{
+    "kind": "books#volumes",
+    "totalItems": 1000000,
+    "items": [
+        {
+            "kind": "books#volume",
+            "id": "FeaoDwAAQBAJ",
+            "etag": "5oY/0T/mRiY",
+            "selfLink": "https:\/\/www.googleapis.com/books/v1/volumes/FeaoDwAAQBAJ",
+            "volumeInfo": {
+                "title": "はじめてのPython AIプログラミング",
+                "authors": ["金城俊哉"],
+                "publisher": "秀和システム",
+                "publishedDate": "2016-11-06",
+                "description": "※この商品はタブレットなど大きいディスプレイを備えた端末で読むことに適しています。また、文字だけを拡大することや、文字列のハイライト、検索、辞書の参照、引用などの機能が使用できません。 いま、さまざまな分野で「機械学習」や「人工知能（AI）」が注目されています。ところで、機械学習やAIの多くが、シンプルで使いやすいプログラミング言語「Python」で作られていることをご存知ですか？ 本書は、無料で使えるPythonを使って、知識ゼロの方でもいきなりAIプログラミングができる世界一簡単なAI入門書です。あなたもPythonの基礎をストーリー形式で楽しく学び、対話型ボット「レイ」の開発を実体験しましょう！",
+                "industryIdentifiers": [
+                    {"type": "OTHER", "identifier": "PKEY:BT000047309100100101900209"}
+                ],
+                "readingModes": {"text": True, "image": True},
+                "pageCount": 302,
+                "printType": "BOOK",
+                "categories": ["Computers"],
+                "maturityRating": "NOT_MATURE",
+                "allowAnonLogging": True,
+                "contentVersion": "2.3.1.0.preview.3",
+                "panelizationSummary": {
+                    "containsEpubBubbles": False,
+                    "containsImageBubbles": False,
+                },
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=FeaoDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=FeaoDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+                },
+                "language": "ja",
+                "previewLink": "http://books.google.co.jp/books?id=FeaoDwAAQBAJ&pg=PA16&dq=python&hl=&cd=1&source=gbs_api",
+                "infoLink": "https://play.google.com/store/books/details?id=FeaoDwAAQBAJ&source=gbs_api",
+                "canonicalVolumeLink": "https://play.google.com/store/books/details?id=FeaoDwAAQBAJ",
+            },
+            "saleInfo": {
+                "country": "JP",
+                "saleability": "FOR_SALE",
+                "isEbook": True,
+                "listPrice": {"amount": 1683, "currencyCode": "JPY"},
+                "retailPrice": {"amount": 1515, "currencyCode": "JPY"},
+                "buyLink": "https://play.google.com/store/books/details?id=FeaoDwAAQBAJ&rdid=book-FeaoDwAAQBAJ&rdot=1&source=gbs_api",
+                "offers": [
+                    {
+                        "finskyOfferType": 1,
+                        "listPrice": {
+                            "amountInMicros": 1683000000,
+                            "currencyCode": "JPY",
+                        },
+                        "retailPrice": {
+                            "amountInMicros": 1515000000,
+                            "currencyCode": "JPY",
+                        },
+                    }
+                ],
+            },
+            "accessInfo": {
+                "country": "JP",
+                "viewability": "PARTIAL",
+                "embeddable": True,
+                "publicDomain": False,
+                "textToSpeechPermission": "ALLOWED",
+                "epub": {
+                    "isAvailable": True,
+                    "acsTokenLink": "http://books.google.co.jp/books/download/%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AEPython_AI%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F-sample-epub.acsm?id=FeaoDwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api",
+                },
+                "pdf": {
+                    "isAvailable": True,
+                    "acsTokenLink": "http://books.google.co.jp/books/download/%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AEPython_AI%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F-sample-pdf.acsm?id=FeaoDwAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api",
+                },
+                "webReaderLink": "http://play.google.com/books/reader?id=FeaoDwAAQBAJ&hl=&source=gbs_api",
+                "accessViewStatus": "SAMPLE",
+                "quoteSharingAllowed": False,
+            },
+            "searchInfo": {
+                "textSnippet": "金城俊哉. はじめよう! <b>Python</b> プログラミング! PC に <b>Python</b> をインストール 01 するさっそく<b>パイソン</b>ラボの隅っこに専用のデスクと PC が与えられました。<b>Python</b> は入ってないので、マニュアルを読みつつインストールしてみることにします。 Windows&nbsp;..."
+            },
+        }
+    ],
+}
